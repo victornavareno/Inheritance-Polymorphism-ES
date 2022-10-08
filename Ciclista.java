53d2e247fc22670f55b4e0a4f6f3d4e9032eb77b
victornavareno <victorandresnm@gmail.com>
2022-10-08 12:47:03 +0200

    Actualización de la clase ciclista, creación esqueleto clase Equipo

--- Ciclista.java
+++ Ciclista.java
@@ -1,33 +1,192 @@
 
 /**
- * Write a description of class Ciclista here.
+ * La clase Ciclista representa a los ciclistas que competirán con sus Bicicletas cada Etapa. La
+ * diferencia entre cada Ciclista viene marcada por la diferente habilidad y energía del Ciclista.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Victor
+ * @version 10.08
  */
 public class Ciclista
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private String nombre;
+    private int habilidad;
+    private int energia;
+    private int resultados;
+    private boolean abandono;
+
+    private Equipo equipo; 
+    private Bicicleta bicicleta;
 
     /**
-     * Constructor for objects of class Ciclista
-     */
-    public Ciclista()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
+     * Constructor de la clase Ciclista
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @param nombre     String con el nombre del ciclista
+     * @param habilidad  Int con el nivel de habilidad del ciclista
+     * @param energia    Int con la energia restante del ciclista durante la competición
+     * @param resultados Int con los resultados en cada etapa
+     * @param abandono   Boolean para indicar si el ciclista ha abandonado o no
+     * @param equipo     Equipo al que pertenece el ciclista
+     * @param bicicleta  Bicicleta asignada al ciclista
      */
-    public int sampleMethod(int y)
+    public Ciclista(String nombre, int habilidad, int energia, int resultados, boolean abandono, Bicicleta bicicleta, Equipo equipo)
     {
-        // put your code here
-        return x + y;
+        this.nombre = nombre;
+        this.habilidad = habilidad;
+        this.energia = energia;
+        this.resultados = resultados;
+        this.abandono = abandono;
+        this.equipo = equipo;
+        this.bicicleta = bicicleta;
     }
+    
+    //MÉTODOS DE ACCESO (get)
+
+    /**
+     * Devuelve el nombre 
+     * 
+     * @return String con el nombre del ciclista
+     */
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+
+    /**
+     * Devuelve la habilidad
+     * 
+     * @return Int con la habilidad del ciclista
+     */
+    public int getHabilidad()
+    {
+        return this.habilidad;
+    }
+
+    /**
+     * Devuelve la energia 
+     * 
+     * @return Int con la energia restante del ciclista
+     */
+    public int getEnergia()
+    {
+        return this.energia;
+    }
+    
+    /**
+     * Devuelve los resultados
+     * 
+     * @return Int con los resultados del ciclista
+     */
+    public int getResultados()
+    {
+        return this.resultados;
+    }
+    
+    /**
+     * Devuelve la energia 
+     * 
+     * @return Int con la energia restante del ciclista
+     */
+    public boolean getAbandono()
+    {
+        return this.abandono;
+    }
+    
+    /**
+     * Devuelve el Equipo
+     * @return Equipo con el equipo asignado
+     */
+    public Equipo getEquipo()
+    {
+        return this.equipo;
+    }
+    
+    /**
+     * Devuelve la Bicicleta 
+     * @return Bicicleta con la bicicleta asignada
+     */
+    public Bicicleta getBicicleta()
+    {
+        return this.bicicleta;
+    }
+    
+    //MÉTODOS MODIFICADORES (set)
+    
+    /**
+     * Asigna el nombre
+     * 
+     * @param nombre String con el nombre del Ciclista
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Asigna el valor de la habilidad
+     * 
+     * @param Int valor de la habilidad del ciclista
+     */
+    public void setHabilidad(int habilidad) {
+        this.habilidad = habilidad;
+    }
+    
+    /**
+     * Asigna el valor de la energia
+     * 
+     * @param Int valor de la energia restante del ciclista
+     */
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+          
+    /**
+     * Asigna el valor de los resultados
+     * 
+     * @param Int valor de los resultados del ciclista
+     */
+    public void setResultados(int resultados) {
+        this.resultados = resultados;
+    }
+           
+    /**
+     * Asigna el estado de abandono 
+     * 
+     * @param Boolean estado de abandono del ciclista en la competicion
+     */
+    public void setAbandono(boolean abandono) {
+        this.abandono = abandono;
+    }
+    
+    /**
+     * Asigna el Equipo
+     * 
+     * @param Equipo asignado al ciclista
+     */
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+        
+    /**
+     * Asigna la Bicicleta
+     * 
+     * @param Bicicleta bicleta asignada al ciclista
+     */
+    public void setBicicleta(Bicicleta bicicleta) {
+        this.bicicleta = bicicleta;
+    }
+    
+    // OTROS MÉTODOS DE LA CLASE CICLISTA:
+
+    /**
+     * Actualiza el estado del ciclista, si no tiene energia abandona 
+     * 
+     */
+    public void actualizarAbandono(){
+        if(this.energia > 0){
+            setAbandono(false);
+        }
+        else{
+            setAbandono(true);
+        }
+    }
+
 }
---
+++ Equipo.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Equipo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Equipo
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Equipo
+     */
+    public Equipo()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
