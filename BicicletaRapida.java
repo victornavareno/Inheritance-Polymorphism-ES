db2d22a0c0b45ac4b1feb225f143e39ae6f9c3ba
victornavareno <victorandresnm@gmail.com>
2022-11-15 20:31:58 +0100

    Clase heredada BicicletaRapida

---
+++ BicicletaRapida.java
@@ -0,0 +1,32 @@
+
+/**
+ * Tienen las mismas características que las Bicicletas normales.
+ *  Además, se caracterizan por tener un campo que indica la velocidad extra.
+ * 
+ * @author Victor
+ * @version 1
+ */
+public class BicicletaRapida extends Bicicleta
+{
+    private double velocidadExtra;
+
+    /**
+     * Constructor de objetos tipo BicicletaRapida
+     */
+    public BicicletaRapida(String nombre, double peso, double velocidadExtra)
+    {
+        super(nombre, peso);
+        this.velocidadExtra = velocidadExtra;
+    }
+    
+    void setVelocidadExtra(double velocidadExtra){
+        this.velocidadExtra = velocidadExtra;
+    }
+    
+    @Override
+    public double calcularVelocidad(double habilidadCiclista, double dificultadEtapa){
+        double velocidad = super.calcularVelocidad(habilidadCiclista, dificultadEtapa);
+        velocidad = velocidad + velocidadExtra;
+        return velocidad;
+    }
+}
